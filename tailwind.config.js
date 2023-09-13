c908061c31cfbfa53c62c3aef6e9d34045abe2b1
manhon95 <manhon95@yahoo.com.hk>
2023-09-13 22:13:55 +0800

    finish introduction

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,23 @@ tailwind.config = {
     container: {
       center: true,
     },
+    extend: {
+      width: {
+        128: "32rem",
+        144: "36rem",
+      },
+      animation: {
+        "bg-pan": "bg-pan-top 8s both infinite",
+      },
+      keyframes: {
+        "bg-pan-top": {
+          "0%, 100%": { "background-position": "top" },
+          "50%": { "background-position": "bottom" },
+        },
+      },
+      backgroundSize: {
+        "size-2": "200% 200%",
+      },
+    },
   },
 };
