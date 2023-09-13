a3d15f79493d6888dc0691036c02e4ca2d87ffde
victor-law <manhon95@yahoo.com.hk>
2023-09-13 17:50:14 +0800

    finish first four sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 tailwind.config = {
   theme: {
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      futura: ["Futura Std", "sans-serif"],
-    },
     extend: {
       spacing: {
+        104: "26rem",
+        112: "28rem",
         128: "32rem",
+      },
+      fontSize: {
+        "2xs": ["0.625rem", "0.75rem"],
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        futura: ["Futura Std", "sans-serif"],
       },
     },
   },
